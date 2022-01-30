e34d5c0f0ad24c56e1ea03626688f2682e1a75a1
framaschool <96589704+framaschool@users.noreply.github.com>
2022-01-30 20:54:11 +0100

    Update normal.sh

--- upgrade/2.1.0/normal.sh
+++ upgrade/2.1.0/normal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ~/ark-core
+cd ~/kapu-core
 pm2 delete all
 git reset --hard
 git pull
@@ -9,8 +9,8 @@ yarn run bootstrap
 yarn run upgrade
 
 # If you do not use Core Commander you can skip this step.
-cd ~/core-commander
+cd ~/kapushell-core
 git reset --hard
 git pull
 git checkout master
-bash commander.sh
+bash kapushellcore.sh
